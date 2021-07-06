716c246b2d2a028bc1e8be18de75d67af4b3fb12
Ivan_Smirnov_Bitbucket <bilivali_2012@mail.ru>
2021-07-06 15:30:25 +0300

    Task_Weapon

---
+++ HomeWorks/Bot.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeWorks
+{
+    class Bot
+    {
+        private readonly Weapon _weapon;
+
+        public Bot(Weapon weapon)
+        {
+            _weapon = weapon;
+        }
+
+        public void OnSeePlayer(Player player)
+        {
+            if (player.Dead) return;
+            _weapon.Fire(player);
+        }
+    }
+}
---
+++ HomeWorks/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWorks
+{
+    class Weapon
+    {
+        private readonly int _damage;
+        private int _bullets;
+        
+        public Weapon(int damage, int bullets)
+        {
+            if(damage < 0 || bullets < 0 )
+                throw new ArgumentOutOfRangeException();
+            
+            _damage = damage;
+            _bullets = bullets;
+        }
+        public void Fire(Player player)
+        {
+            if (_bullets<=0) return;
+            
+            player.TakeDamage(_damage);
+            _bullets--;
+        }
+    }
+}
